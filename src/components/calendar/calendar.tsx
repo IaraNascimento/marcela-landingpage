b59cec4400dac89100e5deedc7a59495762b808a
Iara Nascimento Sztybe <iara.sztybe@gmail.com>
2024-11-08 21:31:42 -0300

    Adicionando agenda do google

---
+++ src/components/calendar/calendar.tsx
@@ -0,0 +1,10 @@
+import "./styles.scss";
+
+export default function ScheduleCalendar() {
+  return (
+    <iframe
+      title="schedule-calendar"
+      src="https://calendar.google.com/calendar/embed?src=700a5078fce58af548d8d1a6fa50949196f3f4f99674ab85de842a7729ec66a2%40group.calendar.google.com&ctz=America%2FSao_Paulo"
+    ></iframe>
+  );
+}
--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,8 +1,14 @@
 import "./styles.scss";
+import ScheduleCalendar from "../../components/calendar/calendar";
 import { useTranslation } from "react-i18next";
 
 export default function Calendar() {
   const { t } = useTranslation();
 
-  return <h1>{t("calendar.title")}</h1>;
+  return (
+    <>
+      <h1>{t("calendar.title")}</h1>
+      <ScheduleCalendar />
+    </>
+  );
 }
